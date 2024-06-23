9ed91e852c2b6dabf67b61924eeccedf6524796a
Josephus67 <jbawah002@st.ug.edu.gh>
2024-06-22 19:45:53 -0700

    added cards for settings's Menus

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-
+import {Card} from 'react-native-paper'
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <View>
-      <Text style={{fontSize: 28, fontWeight: 'bold', textAlign: 'center', marginTop: 50}}>Settings</Text>
-      </View>
-      <View > 
-<Text style={{fontSize: 22, fontWeight: 'bold'}}>Language</Text>
-<Text style={{fontSize: 22, fontWeight: 'bold'}}>My Profile</Text>
-<Text style={{fontSize: 22, fontWeight: 'bold'}}>Contact Us</Text>
-<Text style={{fontSize: 22, fontWeight: 'bold'}}>Change password</Text>
-<Text style={{fontSize: 22, fontWeight: 'bold'}}>Privacy policy</Text>
-      </View>
-    </SafeAreaView>
+
+    <View>
+    <Text style={{fontSize: 28, fontWeight: 'bold', textAlign: 'center', marginTop: 50}}>Settings</Text>
+    </View>
+    <View style={{height: 20}}/>
+    <View> 
+<Card style={styles.card1}>
+<Text style={styles.first_text}>Language </Text>
+<Text style={styles.greater_than}></Text>
+</Card>
+<View style={{height: 20}}/>
+<Card style={styles.card1}>
+<Text style={styles.first_text}>My Profile</Text>
+<Text style={styles.greater_than}>></Text>
+</Card>
+<View style={{height: 20}}/>
+<Card style={styles.card1}>
+<Text style={styles.first_text}>Contact Us</Text>
+<Text style={styles.greater_than}>></Text>
+</Card>
+<View style={{height: 20}}/>
+<Card style={styles.card1}>
+<Text style={styles.first_text}>Change password</Text>
+<Text style={styles.greater_than}>></Text>
+</Card>
+<View style={{height: 20}}/>
+<Card style={styles.card1}>
+<Text style={styles.first_text}>Privacy policy</Text>
+<Text style={styles.greater_than}>></Text>
+</Card>
+    </View>
+    <View style={{height:30}}/>
+    <Text style={{fontSize:25, fontWeight: 'bold'}}>Theme</Text>
+   
+  </SafeAreaView>
+
   );
 }
 
@@ -26,5 +50,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignContent: 'space-evenly',
     marginLeft: 20,
+    
   },
+  first_text: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+  card1: {
+    height: 50,
+    width: '100%',
+   // backgroundColor: '#FF00FF',
+   borderRadius: 10,
+  },
+  greater_than: {
+marginLeft: 320,marginTop: -28, fontWeight: 'bold',fontSize:22
+  }
 });
